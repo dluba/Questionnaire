31ffa5b610439f26dc9b7dbdc8dffe2196ee59e1
dluba <63948864+dluba@users.noreply.github.com>
2021-07-13 13:01:42 +0300

    Add files via upload

---
+++ questionnaire.js
@@ -0,0 +1,8 @@
+var firstName;
+firstName = prompt("Введите имя:");
+var lastName;
+lastName = prompt("Введите фамилию:");
+var agee;
+agee = prompt("Введите возраст:");
+console.log("Пользователь:" + firstName + " " + lastName);
+console.log("Возраст:" + agee);
